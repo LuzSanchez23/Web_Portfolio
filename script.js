61553e9ab72f4b1f75fc102f7de1695baf6eda31
Luz Sanchez <nena_baby23@hotmail.com>
2022-07-10 01:06:40 -0600

    base website is done

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-//Side Nav JS Code
+//Side Nav Code
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
 let navBar = document.querySelector(".navBar");
+let body = document.querySelector("body");
 
-menuBtn.onclick = function(){
+menuBtn.onclick = function() {
     menuBtn.style.opacity = "0";
     menuBtn.style.pointerEvents = "none";
     navBar.classList.add("active");
+    body.style.overflow = "hidden";
 }
-cancelBtn.onclick = function(){
+cancelBtn.onclick = function() {
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
     navBar.classList.remove("active");
+    body.style.overflow = "auto";
 }
 
-//Sticky Nav Menu JS Code
+//Sticky Nav Menu Code
 let nav = document.querySelector("nav");
 let val;
-window.onscroll = function(){
+window.onscroll = function() {
     if(document.documentElement.scrollTop > 20) {
         nav.classList.add("sticky");
     } else {
         nav.classList.remove("sticky")
     }
+}
+//Funtion of Side Navigation when links are clicked//
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener ("click" , ()=> {
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    navBar.classList.remove("active");
+    body.style.overflow = "auto";
+    });
 }
