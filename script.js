e504bdc4cb3f542dfc36a774432e293a25381b23
Luz Sanchez <nena_baby23@hotmail.com>
2022-07-10 23:47:52 -0600

    modifying contact me form

--- script.js
+++ script.js
@@ -37,3 +37,34 @@ for (var i = 0; i < navLinks.length; i++) {
     body.style.overflow = "auto";
     });
 }
+
+//Node Mailer -- Contact Me Form //
+var nodemailer = require('nodemailer');
+
+// Create the transporter with the required configuration for Outlook
+// change the user and pass !
+var transport = nodemailer.createTransport("SMTP", {
+    service: "hotmail",
+    auth: {
+        user: "nena_baby23@hotmail.com",
+        pass: "password"
+    }
+});
+
+// setup e-mail data, even with unicode symbols
+var mailOptions = {
+    from: '"Our Code World " <mymail@outlook.com>', // sender address (who sends)
+    to: 'mymail@mail.com, mymail2@mail.com', // list of receivers (who receives)
+    subject: 'Hello ', // Subject line
+    text: 'Hello world ', // plaintext body
+    html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+
+    console.log('Message sent: ' + info.response);
+});
