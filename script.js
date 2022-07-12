23877458cdebad9589d86a8af9ec23b2c4a5f939
Luz Sanchez <nena_baby23@hotmail.com>
2022-07-11 22:54:27 -0600

    adding about-me bio

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Side Nav Code
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
-let navBar = document.querySelector(".navBar");
-let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+// let body = document.querySelector("body");
 
 menuBtn.onclick = function() {
     menuBtn.style.opacity = "0";
     menuBtn.style.pointerEvents = "none";
     navBar.classList.add("active");
-    body.style.overflow = "hidden";
+    // body.style.overflow = "hidden";
 }
 cancelBtn.onclick = function() {
     menuBtn.style.opacity = "1";
